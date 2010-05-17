c82dc6af8d7666152d5415c3fe14c9a199fbca1d
leandro1604 <leandro1604@d63231ee-c80a-8225-ab00-1a85525f94cd>
2010-05-17 16:15:09 +0000

    Commit Inicial

---
+++ trunk/transport/transport-allocation/src/main/java/br/com/transport/allocation/request/RequestAllocationLocal.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.com.transport.allocation.request;
+
+import javax.ejb.Local;
+
+
+/**
+ * @author leandro.goncalves
+ *
+ */
+@Local
+public interface RequestAllocationLocal extends RequestAllocation {
+
+}
---
+++ trunk/transport/transport-allocation/src/main/java/br/com/transport/allocation/request/RequestAllocationMDB.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package br.com.transport.allocation.request;
+
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.EJB;
+import javax.ejb.MessageDriven;
+import javax.ejb.MessageDrivenContext;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+import br.com.transport.allocation.AllocationLocal;
+import br.com.transport.domain.Freight;
+
+
+/**
+ * @author leandro.goncalves
+ *
+ */
+@MessageDriven(
+		name = "requesAllocationMDB",
+		activationConfig ={
+				@ActivationConfigProperty(
+						propertyName = "destinationType",
+						propertyValue = "javax.jms.Queue"
+				),
+				@ActivationConfigProperty(
+						propertyName = "destination",
+						propertyValue = "/queue/allocationRequest"
+				)		
+		}
+)
+public class RequestAllocationMDB implements MessageListener{
+
+	@Resource
+	private MessageDrivenContext context;
+	
+	@EJB
+	private AllocationLocal allocationLocal;
+	
+	@Override
+	public void onMessage(Message message) {
+		
+		try{
+		
+			ObjectMessage objectMessage =  (ObjectMessage) message;
+			
+			allocationLocal.processAllocation((Freight) objectMessage.getObject(),
+					objectMessage.getStringProperty("MessageID"));
+			
+		}catch (Exception e) {
+			context.getRollbackOnly();
+		}
+		
+	}
+
+}
