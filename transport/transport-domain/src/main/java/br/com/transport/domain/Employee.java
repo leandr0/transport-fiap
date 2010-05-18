70f4718d1e747e40b2b6d36e6a2fce1dcd55c37b
robson.o.d <robson.o.d@d63231ee-c80a-8225-ab00-1a85525f94cd>
2010-05-18 22:33:42 +0000

    add atributos para o entidade employee

--- transport/transport-domain/src/main/java/br/com/transport/domain/Employee.java
+++ transport/transport-domain/src/main/java/br/com/transport/domain/Employee.java
@@ -30,15 +30,50 @@ public class Employee implements EntityBase {
 	private Long id;
 	
 	
+	@Column(name="NAME", nullable = false)
+	private String name;
 	
 	
+	@Column(name="POST" , nullable = false)
+	private Post post;
+
+		
+	public Employee() {}
+
+
+	public Employee(String name, Post post) {
+		super();
+		this.name = name;
+		this.post = post;
+	}
+
 
 	public Long getId() {
 		return id;
 	}
-
+	
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
 
+
+	public String getName() {
+		return name;
+	}
+
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Post getPost() {
+		return post;
+	}
+
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
 }
---
+++ transport/transport-domain/src/main/java/br/com/transport/domain/Post.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package br.com.transport.domain;
+
+/**
+ * @author robson
+ *
+ */
+public enum Post {	
+	DRIVER,
+	AUXILIARY;
+}
