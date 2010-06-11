9e8b17fe9ad54ce678de68a7634b33f722398f50
robson.o.d <robson.o.d@d63231ee-c80a-8225-ab00-1a85525f94cd>
2010-06-11 02:21:25 +0000

    finalizando o modulo de localizacao e alguns ajustes nos outros modulos

---
+++ trunk/transport/transport-truckage/src/main/java/br/com/transport/truckage/ManagerTrackLocal.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package br.com.transport.truckage;
+
+/**
+ * @author robson
+ *
+ */
+public interface ManagerTrackLocal extends ManagerTrack {
+
+}
---
+++ trunk/transport/transport-truckage/src/main/java/br/com/transport/truckage/ManagerTrackRemote.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package br.com.transport.truckage;
+
+/**
+ * @author robson
+ *
+ */
+public interface ManagerTrackRemote extends ManagerTrack {
+
+}
