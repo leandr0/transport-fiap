aa9a459b938e19953c2f8def08291f25fec38db7
leandro1604 <leandro1604@d63231ee-c80a-8225-ab00-1a85525f94cd>
2010-06-16 02:17:01 +0000

    Ajustes

--- trunk/transport/trasnport-ws/src/main/java/br/com/transport/ws/ManagerServiceTranportBean.java
+++ trunk/transport/trasnport-ws/src/main/java/br/com/transport/ws/ManagerServiceTranportBean.java
@@ -3,37 +3,55 @@
  */
 package br.com.transport.ws;
 
+import java.util.List;
+
+import javax.ejb.EJB;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import br.com.transport.allocation.request.RequestAllocationRemote;
+import br.com.transport.allocation.response.ResponseAllocationRemote;
 import br.com.transport.domain.Freight;
+import br.com.transport.domain.vo.ReportVO;
+import br.com.transport.report.ReportRemote;
 
 /**
  * @author leandro.goncalves
  *
  */
-@Stateless
+@Stateless(name = "serviceTransport")
 @Remote(ServiceTransportRemote.class)
-@WebService(serviceName="Counter", portName="CounterPort")
-public class ManagerServiceTranportBean implements ServiceTransportLocal,
-		ServiceTransportRemote {
+@WebService
+public class ManagerServiceTranportBean implements ServiceTransportLocal, ServiceTransportRemote {	
+	
+	@EJB(mappedName="requestAllocation/remote") 
+	private RequestAllocationRemote requestAllocation;
+	
+	@EJB(mappedName="responseAllocation/remote") 
+	private ResponseAllocationRemote responseAllocation;
+	
+	@EJB(mappedName="report/remote") 
+	private ReportRemote report;
 	
 	@Override
 	@WebMethod
 	public String requestAllocation(Freight freight) {
-		
-		
-		
-		return null;
-	}
-
-	@Override
-	public Freight responseAllocation(String idMessage) {
-		// TODO Auto-generated method stub
-		return null;
+		return requestAllocation.requestAllocation(freight);
 	}
 
 	
+	@Override
+	@WebMethod
+	public Freight responseAllocation(String idMessage) {
+		return responseAllocation.responseAllocation(idMessage);
+	}
+
+
+	@Override
+	@WebMethod
+	public List<ReportVO> generateReport() {
+		return report.executeReport();
+	}	
 }
--- trunk/transport/trasnport-ws/src/main/java/br/com/transport/ws/ServiceTransport.java
+++ trunk/transport/trasnport-ws/src/main/java/br/com/transport/ws/ServiceTransport.java
@@ -3,7 +3,10 @@
  */
 package br.com.transport.ws;
 
+import java.util.List;
+
 import br.com.transport.domain.Freight;
+import br.com.transport.domain.vo.ReportVO;
 
 /**
  * @author leandro.goncalves
@@ -14,4 +17,6 @@ public interface ServiceTransport {
 	public String requestAllocation(Freight freight);
 	
 	public Freight responseAllocation(String idMessage);
+	
+	public List<ReportVO> generateReport();
 }
