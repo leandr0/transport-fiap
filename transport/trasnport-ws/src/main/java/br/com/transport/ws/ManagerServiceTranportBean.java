ea8f285dc34a7d6f2bd1c57772f68ce2fbfa4849
leandro1604 <leandro1604@d63231ee-c80a-8225-ab00-1a85525f94cd>
2010-06-16 02:39:28 +0000

    Ajustes

--- transport/trasnport-ws/src/main/java/br/com/transport/ws/ManagerServiceTranportBean.java
+++ transport/trasnport-ws/src/main/java/br/com/transport/ws/ManagerServiceTranportBean.java
@@ -11,11 +11,17 @@ import javax.ejb.Stateless;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
+import br.com.tranport.cadastre.ManagerCadastreRemote;
 import br.com.transport.allocation.request.RequestAllocationRemote;
 import br.com.transport.allocation.response.ResponseAllocationRemote;
+import br.com.transport.domain.Carrier;
+import br.com.transport.domain.Employee;
 import br.com.transport.domain.Freight;
+import br.com.transport.domain.TrackHistory;
 import br.com.transport.domain.vo.ReportVO;
+import br.com.transport.payment.ManagerPaymentBean;
 import br.com.transport.report.ReportRemote;
+import br.com.transport.truckage.ManagerTrackBean;
 
 /**
  * @author leandro.goncalves
@@ -35,6 +41,15 @@ public class ManagerServiceTranportBean implements ServiceTransportLocal, Servic
 	@EJB(mappedName="report/remote") 
 	private ReportRemote report;
 	
+	@EJB(mappedName="cadastre/remote")
+	private ManagerCadastreRemote cadastre;
+	
+	@EJB(mappedName="payment/remote")
+	private ManagerPaymentBean payment;
+	
+	@EJB(mappedName="track/remote")
+	private ManagerTrackBean track;
+	
 	@Override
 	@WebMethod
 	public String requestAllocation(Freight freight) {
@@ -53,5 +68,53 @@ public class ManagerServiceTranportBean implements ServiceTransportLocal, Servic
 	@WebMethod
 	public List<ReportVO> generateReport() {
 		return report.executeReport();
+	}
+	
+	@Override
+	@WebMethod
+	public void addCarrierFleet(List<Carrier> carriers){
+		cadastre.addCarrierFleet(carriers);
+	}
+	
+	@Override
+	@WebMethod
+	public void addEmployee(Employee employee) {
+		cadastre.addEmployee(employee);
+	}
+
+	@Override
+	@WebMethod
+	public Employee findEmployee(Employee employee){
+		return cadastre.findEmployee(employee);
 	}	
+
+	@Override
+	@WebMethod
+	public void removeEmployee(Employee employee) {
+		cadastre.removeEmployee(employee);
+	}
+
+	@Override
+	@WebMethod
+	public Employee updateEmployee(Employee employee){
+		return cadastre.updateEmployee(employee);
+	}
+
+//TODO arrumar esta muito porco
+	@Override
+	@WebMethod
+	public Freight registerPaymentFreight(Long numberFreight, Double value) {
+		try {
+			return payment.registerPaymentFreight(numberFreight, value);
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
+
+	@Override
+	@WebMethod
+	public TrackHistory getTrackHistoryCurrent(Long numberFreight) {
+		return track.getTrackHistoryCurrent(numberFreight);
+	}
 }
--- transport/trasnport-ws/src/main/java/br/com/transport/ws/ServiceTransport.java
+++ transport/trasnport-ws/src/main/java/br/com/transport/ws/ServiceTransport.java
@@ -5,7 +5,10 @@ package br.com.transport.ws;
 
 import java.util.List;
 
+import br.com.transport.domain.Carrier;
+import br.com.transport.domain.Employee;
 import br.com.transport.domain.Freight;
+import br.com.transport.domain.TrackHistory;
 import br.com.transport.domain.vo.ReportVO;
 
 /**
@@ -19,4 +22,18 @@ public interface ServiceTransport {
 	public Freight responseAllocation(String idMessage);
 	
 	public List<ReportVO> generateReport();
+	
+	public void addCarrierFleet(List<Carrier> carriers);
+	
+	public void addEmployee(Employee employee);
+
+	public Employee findEmployee(Employee employee);
+	
+	public void removeEmployee(Employee employee);
+
+	public Employee updateEmployee(Employee employee);
+	
+	public Freight registerPaymentFreight(Long numberFreight, Double value);
+	
+	public TrackHistory getTrackHistoryCurrent(Long numberFreight);
 }
